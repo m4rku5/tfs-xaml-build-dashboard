4b3356551a191be09c6ec667cb0b990b1cae5e12
Markus Lösch <markusloesch@gmail.com>
2017-10-04 19:30:18 +0200

    - added config directory - added config file where in the future the TFS connection will be stored - added json files that contain dummy data until TFS connection is implemented - added endpoint to query build controller list that serves the dummy data

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 
+const fs = require('fs')
+
 app.get('/controllers', (req, res) => {
-  res.send({
-    message: 'hello there!'
-  })
+  const obj = JSON.parse(fs.readFileSync('config/controllers_dummydata.json', 'utf8'))
+  res.send(obj)
 })
 
 app.listen(process.env.PORT || 8081)
