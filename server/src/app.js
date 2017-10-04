e977b2ce8a0feb2a5167c59218c8c543d32ca71c
Markus Lösch <markusloesch@gmail.com>
2017-10-04 21:11:52 +0200

    - added endpoint to get agents per controllers, currently serves dummy data - fixed dummy data to have differnt agents for both dummy controllers

--- server/src/app.js
+++ server/src/app.js
@@ -4,8 +4,21 @@ const app = express()
 const fs = require('fs')
 
 app.get('/controllers', (req, res) => {
-  const obj = JSON.parse(fs.readFileSync('config/controllers_dummydata.json', 'utf8'))
-  res.send(obj)
+  try {
+    const controllers = JSON.parse(fs.readFileSync('config/controllers_dummydata.json', 'utf8'))
+    res.send(controllers)
+  } catch (err) {
+    res.status(400).send(`Cannot get controllers: ${err.message}`)
+  }
+})
+
+app.get('/controllers/:agentId', function (req, res) {
+  try {
+    const agents = JSON.parse(fs.readFileSync(`config/agents_dummydata_${req.params.agentId}.json`, 'utf8'))
+    res.send(agents)
+  } catch (err) {
+    res.status(400).send(`Cannot get agents for controller with Id ${req.params.agentId}: ${err.message}`)
+  }
 })
 
 app.listen(process.env.PORT || 8081)
