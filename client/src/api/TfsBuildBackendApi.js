9079be7112400ecc278a4e50c603df83023d784f
Markus Lösch <markusloesch@gmail.com>
2017-10-11 23:03:21 +0200

    added client side api that is used to query data from the backend server

--- client/src/api/TfsBuildBackendApi.js
+++ client/src/api/TfsBuildBackendApi.js
@@ -1,34 +1,18 @@
 import axios from 'axios'
 
-function TfsApiException (message) {
-  this.message = message
-  this.name = TfsApiException
-}
-
 const api = axios.create({
   baseURL: `http://localhost:8081/`
 })
 
 export default {
   getControllers: () => {
-    var controllers
-    api.get('Controllers').then((result) => {
-      controllers = result.data
-    })
-    .err((err) => {
-      throw new TfsApiException(`Unable to query controllers: ${err}`)
-    })
-    return controllers
+    return api.get('Controllers')
   },
+
   getAgents: (controllerId) => {
-    var controllers
-    api.get(`Controllers/${controllerId}`).then((result) => {
-      controllers = result.data
-    })
-    .err((err) => {
-      throw new TfsApiException(`Unable to query controllers: ${err}`)
-    })
-    return controllers
+    const agentResource = `Controllers/${controllerId}`
+    console.log(`querying resource ${agentResource}`)
+    return api.get(agentResource)
   }
 
 }
